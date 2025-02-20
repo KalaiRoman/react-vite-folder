58daec7ba9b877db40af2f4d7d1a3e0e505bc03d
kalai <kalai@cdp360.com>
2025-02-20 17:12:42 +0530

    tailwind implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
 
 function App() {
   return (
-    <div>
-      kalai
+    <div className="text-blue-500 flex gap-2 p-2 flex-col xs:flex-col sm:flex-row bg-amber-300">
+      <div className="w-full sm:w-1/2 lg:bg-red-300 p-4">left</div>
+      <div className="w-full sm:w-1/2 p-4">right</div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,39 @@
 import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig(() => ({
+  server: {
+    port: 3002,
+  },
   esbuild: {
-    jsx: "automatic", // Enable JSX support
-    loader: "tsx", // Can also be "jsx"
-    include: ["src/**/*.{js,jsx,ts,tsx}"], // Ensure it applies to all JS/TS files
+    jsx: "automatic",
+    loader: "tsx",
+    include: ["src/**/*.{js,jsx,ts,tsx}"],
+  },
+  plugins: [tailwindcss()],
+  css: {
+    preprocessorOptions: {
+      tailwindcss: {
+        theme: {
+          extend: {
+            screens: {
+              xs: "475px", // Extra Small Devices
+              sm: "640px", // Small Devices
+              md: "768px", // Medium Devices
+              lg: "1024px", // Large Devices
+              xl: "1280px", // Extra Large Devices
+              "2xl": "1536px", // 2X Large Devices
+            },
+            colors: {
+              primary: "#1E40AF", // Custom primary blue
+              secondary: "#9333EA", // Custom purple
+              accent: "#F59E0B", // Custom yellow
+              background: "#F3F4F6", // Light gray
+              dark: "#111827", // Dark theme
+            },
+          },
+        },
+      },
+    },
   },
 }));
